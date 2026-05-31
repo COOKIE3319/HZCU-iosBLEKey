c491b08feccc33aef56f1692b6300f0e23acbf50
COOKIE3319 <cookie3319@icloud.com>
2026-05-31 13:26:14 +0800

    Initial Commit

---
+++ HZCU/HZCUApp.swift
@@ -0,0 +1,17 @@
+//
+//  HZCUApp.swift
+//  HZCU
+//
+//  Created by DataNeko.IO on 2026/05/31.
+//
+
+import SwiftUI
+
+@main
+struct HZCUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HZCUTests/HZCUTests.swift
@@ -0,0 +1,17 @@
+//
+//  HZCUTests.swift
+//  HZCUTests
+//
+//  Created by DataNeko.IO on 2026/05/31.
+//
+
+import Testing
+@testable import HZCU
+
+struct HZCUTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
